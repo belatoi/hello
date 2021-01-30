1a389bb1f9870f199c5cb2a4f53ee3ae3a3696e8
Thomas Nguyen <thomas.nguyen@be.com.vn>
2021-01-30 19:58:49 +0700

    create new screen main hello

--- lib/hello.dart
+++ lib/hello.dart
@@ -1,7 +1,27 @@
 library hello;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class MainHello extends StatefulWidget {
+  @override
+  _MainHelloState createState() => _MainHelloState();
+}
+
+class _MainHelloState extends State<MainHello> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Hello"),
+      ),
+      backgroundColor: Colors.red,
+      body: Container(),
+    );
+  }
 }
